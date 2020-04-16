42833ac9597aafc481f91bac5e330b5288f4a3c9
Sajan-poudel <sajanpoudel32@gmail.com>
2020-04-16 17:41:20 +0530

    tables and details

--- app.js
+++ app.js
@@ -61,7 +61,21 @@ app.post('/auth', (req, res)=>{
             if(result.length > 0){
                 req.session.loggedin = true;
                 req.session.results = result;
-                res.redirect('/home');
+                console.log(result);
+                connection.query('DELETE FROM active_user', (e, ress)=>{
+                    if(e){
+                        res.send({error: "something wrong in deleting"});
+                    }else{
+                        connection.query('INSERT INTO active_user VALUES (?, ?, ?, ?, ?, ?)', [result[0].name, result[0].email_id, result[0].password, result[0].connect_no, result[0].latitude_1, result[0].longitude_1], (er, r)=>{
+                            if(er){
+                                res.send({error: "error in inserting the values in active users"});
+                            }else{
+                                console.log
+                                res.redirect('/home');
+                            }
+                        });
+                    }
+                });
             }else{
                 res.send("Incorrect email or password");
             }
@@ -94,12 +108,27 @@ app.post('/registration', (req, res)=>{
 
 app.post('/query', (req, res)=>{
     connection.query(req.body.searchquery, (err, result)=>{
-        console.log(result);
+        // console.log(result);
         res.render('home', {obj : result});
     });
 })
 
-app.get('/auth', (res, req)=>{
+app.get('/details', (req, res)=>{
+    console.log(res.body);
+    connection.query("SELECT * FROM restaurants", (err, result)=>{
+        if(err){
+            res.send({error : err});
+        }else{
+            res.render('details', {obj: result});
+        }
+    });
+});
+
+app.post('/details', (req, res)=>{
+    console.log(req.body);
+});
+
+app.get('/auth', (req, res)=>{
     console.log(res.body);
 });
 
@@ -108,10 +137,10 @@ app.get('/register', (req, res)=>{
 });
 
 app.get('/home', (req, res)=>{
-    // if(req.session.loggedin){
+    if(req.session.loggedin){
         var obj = [];
         res.render('home', {obj: obj});
-    // }
+    }
 });
 
 app.listen(port, ()=>{
